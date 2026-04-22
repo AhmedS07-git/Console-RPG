a16f4031181f9fbbf80673d9b5291d73760c6953
AhmedS07-git <30033664@cc.peralta.edu>
2026-04-22 10:36:58 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,12 +3,19 @@
 
 #include <string>
 
+#include "Inventory.h"
+
 class Player { 
     private:
     std::string name;
     int health;
     int maxHealth;
     int attackPower;
+
+    Inventory inventory;
+
+    int x;
+    int y;
 
 public:
 Player();
@@ -24,5 +31,15 @@ int getMaxHealth() const;
 int getAttackPower() const;
 
 void displayStatus() const;
+
+void addItem(const Item& item);
+void showInventory() const;
+
+int getX() const;
+int getY() const;
+void setX(int x);
+void setY(int y);
+
 };
+
 #endif
